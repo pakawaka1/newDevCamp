8e1ca62898dc9c5bbe07fa6b08a25e1dd8dd4494
pakawaka1 <davidcervantesdev@gmail.com>
2020-02-05 14:35:31 -0800

    added mongoDB connectivity with mongoose

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express');
-//dotenv for env - not needed for vehicles
-
 const dotenv = require('dotenv');
-
-// logger from npm (not needed for vehicles)
 const morgan = require('morgan');
-// logger middleware (not needed for vehicles)
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// Route files
-
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load env vars (not needed for vehicles)
 dotenv.config({ path: './config/config.env' });
 
+// Connect to Database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
+// dev logger middleware (not needed for vehicles)
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -29,7 +25,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exits process
+  server.close(() => process.exit(1));
+});
