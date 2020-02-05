1b18d17e6b8cfb372f63fc6bb0dad8b948f9aca9
pakawaka1 <davidcervantesdev@gmail.com>
2020-02-05 13:04:54 -0800

    feat: added logging middleware

--- server.js
+++ server.js
@@ -1,43 +1,31 @@
 const express = require('express');
+//dotenv for env - not needed for vehicles
+
 const dotenv = require('dotenv');
 
-// Load env vars
+// logger from npm (not needed for vehicles)
+const morgan = require('morgan');
+// logger middleware (not needed for vehicles)
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Route files
+
+const bootcamps = require('./routes/bootcamps');
+
+// Load env vars (not needed for vehicles)
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-// routes
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-// get all bootcamps
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+// Mount routers
 
-// get one bootcamp
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-// create new bootcramp
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-// update one bootcamp
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-// delete one bootcamp
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
