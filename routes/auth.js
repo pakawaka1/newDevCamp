9e1af25bc831fa0e83251af27f221bfb765cdc23
pakawaka1 <davidcervantesdev@gmail.com>
2020-03-31 18:25:34 -0700

    feat:  added updated details and password for users

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ const {
   logout,
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails,
+  updatePassword
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -16,6 +18,8 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
